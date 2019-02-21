ab68b234c65fac8b0be49566901650d334e42530
Lars Andersen Bratholm <larsbratholm@gmail.com>
2019-02-21 18:15:13 +0000

    Updated parser

--- portfolio/utils.py
+++ portfolio/utils.py
@@ -28,16 +28,8 @@ def _is_integer(x):
 def is_positive_integer_or_zero(x):
     return (not is_array_like(x) and _is_integer(x) and x >= 0)
 
-def is_none(x):
-    return isinstance(x, type(None))
-
 def is_bool(x):
     return isinstance(x, bool)
 
 def is_positive_array(x):
     return (is_array_like(x) and (x>0).all())
-
-# custom exception to raise when we intentionally catch an error
-class InputError(Exception):
-    pass
-
